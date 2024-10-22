dad9a3b87dc7ec34aeacf78eec301c41a80ae33e
MinhNghiaTran01 <tranminhnghia19061993x@gmail.com>
2024-10-22 11:29:49 +0700

    first commit

---
+++ src/test/java/com/javaweb/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaweb.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CourseApplicationTests {
+
+	@org.junit.jupiter.api.Test
+    public void contextLoads() {
+	}
+
+}
